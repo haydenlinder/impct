8f1e06e517d034e45ccbc3971c93464c7e09c405
haydenlinder <haydenlinder@gmail.com>
2020-06-12 15:25:57 -0700

    Updates

---
+++ precache-manifest.c6dd711bc09f0dc5d0fc0a2bc0682620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa37207c3881f57d37ed64d1faf9584",
+    "url": "/impct/index.html"
+  },
+  {
+    "revision": "4346a8d48e8b4e91aee3",
+    "url": "/impct/static/css/main.72c7c085.chunk.css"
+  },
+  {
+    "revision": "611b0dd58716a7fa48a6",
+    "url": "/impct/static/js/2.3435d8b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/impct/static/js/2.3435d8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4346a8d48e8b4e91aee3",
+    "url": "/impct/static/js/main.d3e87906.chunk.js"
+  },
+  {
+    "revision": "c4b2e91993189424ec80",
+    "url": "/impct/static/js/runtime-main.7a3889b0.js"
+  }
+]);
