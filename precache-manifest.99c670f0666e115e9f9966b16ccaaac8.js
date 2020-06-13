d0313635bb67e68d61c9fe51f4e1e141bffc2126
haydenlinder <haydenlinder@gmail.com>
2020-06-12 19:39:44 -0700

    Updates

---
+++ precache-manifest.99c670f0666e115e9f9966b16ccaaac8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af85821505ea1c56d4b86e67b68935fd",
+    "url": "/impct/index.html"
+  },
+  {
+    "revision": "8273745264910536fdb4",
+    "url": "/impct/static/css/main.25172881.chunk.css"
+  },
+  {
+    "revision": "afaad19d96dd621ce88f",
+    "url": "/impct/static/js/2.25cc09a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/impct/static/js/2.25cc09a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8273745264910536fdb4",
+    "url": "/impct/static/js/main.33b81d74.chunk.js"
+  },
+  {
+    "revision": "c4b2e91993189424ec80",
+    "url": "/impct/static/js/runtime-main.7a3889b0.js"
+  },
+  {
+    "revision": "1ed7bc7d1d9424c36cc9b612425a5b8d",
+    "url": "/impct/static/media/coffee1.1ed7bc7d.png"
+  },
+  {
+    "revision": "c906672cf9d39ba6a5a7be5f1e18cd51",
+    "url": "/impct/static/media/coffee2.c906672c.png"
+  },
+  {
+    "revision": "7c08ded05db1463edb334ee9a5e81ae2",
+    "url": "/impct/static/media/coffee3.7c08ded0.png"
+  },
+  {
+    "revision": "07a89538421a3ae0ece23c407f6ad1d4",
+    "url": "/impct/static/media/coffee4.07a89538.jpg"
+  },
+  {
+    "revision": "f7a795b6494ff249377a1b884f6f3af8",
+    "url": "/impct/static/media/coffee5.f7a795b6.png"
+  },
+  {
+    "revision": "2d44032aff1af7ee81fdcd528705727a",
+    "url": "/impct/static/media/woman1.2d44032a.jpg"
+  }
+]);
